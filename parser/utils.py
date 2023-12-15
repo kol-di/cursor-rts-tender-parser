bd39c5c76662da867394f073edccc7c28f9fc905
Dmitry Kolobaev <kol-di@yandex.ru>
2023-12-15 19:14:16 +0300

    moved xpath_soup to utils

---
+++ parser/utils.py
@@ -0,0 +1,19 @@
+def xpath_soup(element):
+    """
+    Generate xpath of soup element
+    :param element: bs4 text or node
+    :return: xpath as string
+    """
+    components = []
+    child = element if element.name else element.parent
+    for parent in child.parents:  # type: bs4.element.Tag
+        siblings = parent.find_all(child.name, recursive=False)
+        components.append(
+            child.name if 1 == len(siblings) else '%s[%d]' % (
+                child.name,
+                next(i for i, s in enumerate(siblings, 1) if s is child)
+                )
+            )
+        child = parent
+    components.reverse()
+    return '/%s' % '/'.join(components)
