ecd44b45ce95e3afa8e5073ec148ff03555c397d
Dmitry Kolobaev <kol-di@yandex.ru>
2023-12-18 17:05:27 +0300

    result collector page navigation

--- parser/collector.py
+++ parser/collector.py
@@ -2,7 +2,10 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import re
+
+from .utils import xpath_soup
 
 
 def final_page():
@@ -14,23 +17,23 @@ def next_page(driver, page_num):
     Jumps to the next page
     return: True/False if was able to jump to the next page
     """
-    print('inside next_page')
     pager_interact = driver.find_element(By.ID, 'pager')
-    WebDriverWait(pager_interact, 10).until(EC.visibility_of_all_elements_located((By.TAG_NAME, "li")))
+    try:
+        WebDriverWait(pager_interact, 10).until(EC.visibility_of_all_elements_located((By.TAG_NAME, "li")))
+    except TimeoutException:    # no pages hence empty search result
+        return False
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     li_pages = soup.find('ul', {'id': 'pager'}).find_all('li')
-    print(soup.find('ul', {'id': 'pager'}))
-    print(f'li_pages {li_pages}')
     for page in li_pages:
-        print('found some pages')
         try:
             next_link = page.find('a', {'class': 'page-link'})
-            print(f'found text {next_link.get_text()}')
-            if page.find('a', {'class': 'page-link'}).get_text() == str(page_num):
-                # next_link.click()
-                WebDriverWait(page, 10).until(
-                    EC.element_to_be_clickable((By.TAG_NAME, "a"))).click()
+            if next_link.get_text() == str(page_num):
+                current_url = driver.current_url
+                WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, xpath_soup(next_link)))).click()
+                # only return when successfully redirected
+                WebDriverWait(driver, 10).until(lambda driver: driver.current_url != current_url)
                 return True
         except AttributeError:
             pass
@@ -56,10 +59,10 @@ def collect_page_contents(driver):
 
 
 def collect(driver):
+
+    collect_page_contents(driver)
     next_page_numb = 2
     while next_page(driver, next_page_numb):
-        
         collect_page_contents(driver)
-        # move_to_next_page(driver)
         next_page_numb += 1
         # break
