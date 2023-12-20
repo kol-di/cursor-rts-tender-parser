ad7cc59abaea3eedcd9acd64daa5c919e80038d9
Dmitry Kolobaev <kol-di@yandex.ru>
2023-12-20 20:22:31 +0300

    headless + popup blocking

--- parser/driver.py
+++ parser/driver.py
@@ -9,11 +9,19 @@ WEBSITE_URL = r'https://www.rts-tender.ru/'
 def init_driver():
     service = webdriver.ChromeService(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
+
+    # ignore ssl handshake
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
     options.add_argument('--ignore-urlfetcher-cert-requests')
     options.add_argument('--allow-insecure-localhost')
     options.add_argument('--ignore-certificate-errors-spki-list')
+
+    #disable popups
+    options.add_argument('--disable-notifications')
+    options.add_argument('--disable-popup-blocking')
+
+    options.add_argument('--headless')
     options.accept_insecure_certs = True
     driver = webdriver.Chrome(service=service, options=options)
     driver.get(WEBSITE_URL)
